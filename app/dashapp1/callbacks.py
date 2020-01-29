cdee4be55449bb53adf71b7efee79a3e9aca4fc0
Rellika Kisyula <kisyular@msu.edu>
2020-01-28 20:21:47 -0500

    Simple Graph

--- app/dashapp1/callbacks.py
+++ app/dashapp1/callbacks.py
@@ -25,11 +25,10 @@ def register_callbacks(dashapp):
             'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
         }
 
-
+#
 # def get_datasets():
 #     """Return previews of all CSVs saved in /data directory."""
-#     p = Path('.')
-#     data_filepath = list(p.glob('data/*.csv'))
+#
 #     arr = ['This is an example Plot.ly Dash App.']
 #     for index, csv in enumerate(data_filepath):
 #         df = pd.read_csv(data_filepath[index]).head(10)
@@ -42,72 +41,72 @@ def register_callbacks(dashapp):
 #         )
 #         arr.append(table_preview)
 #     return arr
-
-
-# Store url
-def get_url(app, website_url, website_name):
-    with app.app_context():
-        main_list = []
-        try:
-            # Make the request and check object type
-            r = requests.get(website_url)
-            # Extract HTML from Response object and print
-            html = r.text
-            # Create a BeautifulSoup object from the HTML
-            soup = BeautifulSoup(html, "html.parser")
-            # Get the text out of the soup tag p and print it
-            text = soup.get_text()
-            text = text.split('\n')
-            for t in text:
-                if len(t) > 0:
-                    tlist = t.split('END')
-                    main_list.append(tlist)
-            create_main_list(main_list, website_name)
-            # create_dframe(main_list)
-            return main_list
-        except:
-            pass
-
-
-def read_ip_info(ip):
-    reader = geoip2.database.Reader('./GeoLite2-City.mmdb')
-    response = reader.city(ip)
-    return [response.country.name, response.subdivisions.most_specific.name,
-            response.city.name,
-            response.postal.code, response.location.latitude,
-            response.location.longitude
-            ]
-
-
-def create_main_list(llist, website_name):
-    main_list = []
-    for val in llist:
-        combine = val[1:] + read_ip_info(val[0])
-        v_info = ScrappedData.query.filter_by(unique_time=combine[2],
-                                              website_name=website_name).first()
-        if v_info is None:
-            v_in = ScrappedData(browser=combine[0],
-                                time=combine[1],
-                                unique_time=combine[2],
-                                ip_request=val[0],
-                                country=combine[3],
-                                region=combine[4],
-                                city=combine[5],
-                                zipcode=combine[6],
-                                latitude=combine[7],
-                                longitude=combine[8],
-                                website_name=website_name)
-            db.session.add(v_in)
-            db.session.commit()
-        main_list.append(combine)
-    df = pd.DataFrame(main_list)
-    return df
-
-
-def run_test():
-    website_url = 'https://kisyula.com/gud.txt'
-    website_name = 'Rellika'
-    threading.Thread(target=get_url,
-                     args=(current_app._get_current_object(), website_url,
-                           website_name)).start()
+#
+#
+# # Store url
+# def get_url(app, website_url, website_name):
+#     with app.app_context():
+#         main_list = []
+#         try:
+#             # Make the request and check object type
+#             r = requests.get(website_url)
+#             # Extract HTML from Response object and print
+#             html = r.text
+#             # Create a BeautifulSoup object from the HTML
+#             soup = BeautifulSoup(html, "html.parser")
+#             # Get the text out of the soup tag p and print it
+#             text = soup.get_text()
+#             text = text.split('\n')
+#             for t in text:
+#                 if len(t) > 0:
+#                     tlist = t.split('END')
+#                     main_list.append(tlist)
+#             create_main_list(main_list, website_name)
+#             # create_dframe(main_list)
+#             return main_list
+#         except:
+#             pass
+#
+#
+# def read_ip_info(ip):
+#     reader = geoip2.database.Reader('./GeoLite2-City.mmdb')
+#     response = reader.city(ip)
+#     return [response.country.name, response.subdivisions.most_specific.name,
+#             response.city.name,
+#             response.postal.code, response.location.latitude,
+#             response.location.longitude
+#             ]
+#
+#
+# def create_main_list(llist, website_name):
+#     main_list = []
+#     for val in llist:
+#         combine = val[1:] + read_ip_info(val[0])
+#         v_info = ScrappedData.query.filter_by(unique_time=combine[2],
+#                                               website_name=website_name).first()
+#         if v_info is None:
+#             v_in = ScrappedData(browser=combine[0],
+#                                 time=combine[1],
+#                                 unique_time=combine[2],
+#                                 ip_request=val[0],
+#                                 country=combine[3],
+#                                 region=combine[4],
+#                                 city=combine[5],
+#                                 zipcode=combine[6],
+#                                 latitude=combine[7],
+#                                 longitude=combine[8],
+#                                 website_name=website_name)
+#             db.session.add(v_in)
+#             db.session.commit()
+#         main_list.append(combine)
+#     df = pd.DataFrame(main_list)
+#     return df
+#
+#
+# def run_test():
+#     website_url = 'https://kisyula.com/gud.txt'
+#     website_name = 'Rellika'
+#     threading.Thread(target=get_url,
+#                      args=(current_app._get_current_object(), website_url,
+#                            website_name)).start()
 
--- app/dashapp2/layout.py
+++ app/dashapp2/layout.py
@@ -1,7 +1,44 @@
 import dash_core_components as dcc
 import dash_html_components as html
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 layout = html.Div([
+    html.Div(style={'backgroundColor': colors['background']}, children=[
+        html.H1(
+            children='Hello Dash',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+        html.Div(children='Dash: A web application framework for Python.',
+                 style={
+                     'textAlign': 'center',
+                     'color': colors['text']
+                 }),
+        dcc.Graph(
+            id='Graph1',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar',
+                     'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar',
+                     'name': u'Montréal'},
+                ],
+                'layout': {
+                    'plot_bgcolor': colors['background'],
+                    'paper_bgcolor': colors['background'],
+                    'font': {
+                        'color': colors['text']
+                    }
+                }
+            }
+        )
+    ]),
     html.H1('Stock Tickers'),
     dcc.Dropdown(
         id='my-dropdown',
